0b3fc4cb7ad767406ffa2725d61f82006de467a0
harsha_Wije <harshawije99@gmail.com>
2022-10-13 00:33:04 +0530

    Code cleanup

--- dist/API/Customers/List.js
+++ dist/API/Customers/List.js
@@ -3,7 +3,5 @@ import { GetListAsync } from "../../Services/GetListAsyncSpecification.js";
 const router = express.Router();
 export const getCustomerList = router.get('/', (req, res) => {
     GetListAsync().then(x => res.send(x));
-    //cleconsole.log(customerList);
-    //res.send(customerList);
 });
 //# sourceMappingURL=List.js.map
--- src/API/Customers/List.ts
+++ src/API/Customers/List.ts
@@ -4,6 +4,4 @@ const router = express.Router();
 
 export const getCustomerList = router.get('/', (req: Request, res: Response) => {
     GetListAsync().then(x => res.send(x));
-    //cleconsole.log(customerList);
-    //res.send(customerList);
 })
